9fb49bf674924940d103f9928a7188c7dafebcd8
Giovanni Ricca <giovanniricca@proton.me>
2025-12-29 12:35:12 +0100

    tiro: Decommonize security patch level
    Change-Id: I9d3a410aa8e0765c540e836ec40369c91dc4a2d2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,4 +12,8 @@ TARGET_KERNEL_CONFIG += \
 # Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 
+# Security patch
+BOOT_SECURITY_PATCH := 2025-11-01
+VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
+
 include vendor/nubia/tiro/BoardConfigVendor.mk
